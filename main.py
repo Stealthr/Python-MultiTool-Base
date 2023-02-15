ce17422771bf481cdefe6e4d69e932a6219413f4
Stealth <42773083+Stealthr@users.noreply.github.com>
2023-02-15 11:10:28 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+from rgbprint import gradient_print
+from rgbprint import Color 
+import time
+import os
+
+
+# if thyis shit sucks don't blame me im tired
+
+
+# the ascii. change it to smth
+gradient_print(r"""
+ ██▀███   ██▓▒███████▒▓█████    ▄▄▄█████▓ ▒█████   ▒█████   ██▓     ██ ▄█▀ ██▓▄▄▄█████▓
+▓██ ▒ ██▒▓██▒▒ ▒ ▒ ▄▀░▓█   ▀    ▓  ██▒ ▓▒▒██▒  ██▒▒██▒  ██▒▓██▒     ██▄█▒ ▓██▒▓  ██▒ ▓▒
+▓██ ░▄█ ▒▒██▒░ ▒ ▄▀▒░ ▒███      ▒ ▓██░ ▒░▒██░  ██▒▒██░  ██▒▒██░    ▓███▄░ ▒██▒▒ ▓██░ ▒░
+▒██▀▀█▄  ░██░  ▄▀▒   ░▒▓█  ▄    ░ ▓██▓ ░ ▒██   ██░▒██   ██░▒██░    ▓██ █▄ ░██░░ ▓██▓ ░ 
+░██▓ ▒██▒░██░▒███████▒░▒████▒     ▒██▒ ░ ░ ████▓▒░░ ████▓▒░░██████▒▒██▒ █▄░██░  ▒██▒ ░ 
+░ ▒▓ ░▒▓░░▓  ░▒▒ ▓░▒░▒░░ ▒░ ░     ▒ ░░   ░ ▒░▒░▒░ ░ ▒░▒░▒░ ░ ▒░▓  ░▒ ▒▒ ▓▒░▓    ▒ ░░   
+  ░▒ ░ ▒░ ▒ ░░░▒ ▒ ░ ▒ ░ ░  ░       ░      ░ ▒ ▒░   ░ ▒ ▒░ ░ ░ ▒  ░░ ░▒ ▒░ ▒ ░    ░    
+  ░░   ░  ▒ ░░ ░ ░ ░ ░   ░        ░      ░ ░ ░ ▒  ░ ░ ░ ▒    ░ ░   ░ ░░ ░  ▒ ░  ░      
+   ░      ░    ░ ░       ░  ░                ░ ░      ░ ░      ░  ░░  ░    ░           
+             ░                                                                         
+    """, start_color=Color.aqua_marine, end_color=Color.peach_puff)
+gradient_print("Sneezes On Network", start_color=Color.aqua_marine, end_color=Color.peach_puff)
+
+time.sleep(0.2)
+# if you're gonna add a login system add it here.
+gradient_print("[+] Welcome.", start_color=Color.aqua_marine, end_color=Color.peach_puff )
+
+# logic
+gradient_print("[!] Load Succsess.", start_color=Color.aqua_marine, end_color=Color.peach_puff)
+# modify this shit to be whatever you want, 
+gradient_print("╔════════════════════════╦════════════════════════════════════════════════════════════╦═════════════════════╗", start_color=Color.aqua_marine, end_color=Color.peach_puff)
+gradient_print("║ > Local Tools          ║ > Remote Tools                                             ║ > Utilities         ║", start_color=Color.aqua_marine, end_color=Color.peach_puff)
+gradient_print("╠══╦═════════════════════╬══╦══════════════════════╦══╦═══════════════════════════════╬══╦══════════════════╣", start_color=Color.aqua_marine, end_color=Color.peach_puff)
+gradient_print("║01║ Delete Cache        ║02║ Port scanner         ║03║ Vulner  Scanner               ║04║ best.best.best   ║", start_color=Color.aqua_marine, end_color=Color.peach_puff)
+gradient_print("║05║ Scan Local Networks ║06║ TCP UDP , Flood      ║07║ Site Visit Bot NProx          ║08║ best.best.best   ║", start_color=Color.aqua_marine, end_color=Color.peach_puff)
+gradient_print("║09║ AES Encryptor W Key ║10║ DC API Abusers       ║11║ Network Tunnel NoPFW          ║12║ best.best.best   ║", start_color=Color.aqua_marine, end_color=Color.peach_puff)
+gradient_print("║13║ Aes Decryptor W key ║14║ Static Binerys       ║15║ Forum List. 15/02/22          ║16║ best.best.best   ║", start_color=Color.aqua_marine, end_color=Color.peach_puff)
+gradient_print("║17║ Drive Zero Tool ? . ║18║ WebHook Flooder      ║19║ PGP Encrypt Messages          ║20║ Credits          ║", start_color=Color.aqua_marine, end_color=Color.peach_puff)
+gradient_print("╚══╩═════════════════════╩══╩══════════════════════╩══╩═══════════════════════════════╩══╩══════════════════╝", start_color=Color.aqua_marine, end_color=Color.peach_puff)
+# yooo unrelated but this color fire
+choice = input
+gradient_print = input("[?] Choose your option: ")
+
+# you use if statements for it. i can't be aseed doing it so its just like
+
+# if choice == "01" or "1" then:
+# ur code but indented
+# then theres an elseif or somethnig idk python isnt main lang
+
+# yeah it's halfd assed and easy and legitamately just print statements but it's a base what ddi you excpect + im like 2 days sleep deprived
+
